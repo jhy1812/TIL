028f556c9905443bc08b689f9cebf16610a11dde
정호윤 <jhy1812@naver.com>
2023-01-13 14:36:14 +0900

    fifth commit

--- tsttt.py
+++ tsttt.py
@@ -6,14 +6,14 @@ import requests
 # print(result['age'])
 # print(result.get('age'))  #get 그냥 요청할 때와 차이 - key값이 없으면 null 반환
 
-hoicha=input()
+# hoicha=input()
 
-url2='https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo='+hoicha
-result2=requests.get(url2).json()
+# url2='https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo='+hoicha
+# result2=requests.get(url2).json()
 
-for i in range(1,7):
-    words="drwtNo"+str(i)
-    print(result2.get(words), end=' ')
+# for i in range(1,7):
+#     words="drwtNo"+str(i)
+#     print(result2.get(words), end=' ')
 
 # print(result2['drwtNo1'], end=' ')
 # print(result2['drwtNo2'], end=' ')
@@ -21,3 +21,6 @@ for i in range(1,7):
 # print(result2['drwtNo4'], end=' ')
 # print(result2['drwtNo5'], end=' ')
 # print(result2['drwtNo6'])
+a=3
+print(a)
+print(a)
