920c5cba466f905a148b4a45d66ec7786f90cb6d
정호윤 <jhy1812@naver.com>
2023-01-13 14:18:55 +0900

    third commit

---
+++ tsttt.py
@@ -0,0 +1,23 @@
+import requests
+
+# url='https://api.agify.io/?name=hoyun'
+# result=requests.get(url).json()
+# print(result)
+# print(result['age'])
+# print(result.get('age'))  #get 그냥 요청할 때와 차이 - key값이 없으면 null 반환
+
+hoicha=input()
+
+url2='https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo='+hoicha
+result2=requests.get(url2).json()
+
+for i in range(1,7):
+    words="drwtNo"+str(i)
+    print(result2.get(words), end=' ')
+
+# print(result2['drwtNo1'], end=' ')
+# print(result2['drwtNo2'], end=' ')
+# print(result2['drwtNo3'], end=' ')
+# print(result2['drwtNo4'], end=' ')
+# print(result2['drwtNo5'], end=' ')
+# print(result2['drwtNo6'])
