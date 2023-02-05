aed4c246506f5eeef5ed929594879e1efb9fb6fb
정호윤 <jhy1812@naver.com>
2023-02-05 23:06:04 +0900

    modified

--- SWEA/Swea_D4_back_to_room.py
+++ SWEA/Swea_D4_back_to_room.py
@@ -27,33 +27,3 @@ for tc in range(1, T+1):
             maxx = time[i]
 
     print(f'#{tc} {maxx}')
-        
-    
-# T = int(input())
-
-# for tc in range(1, T+1):
-#     N = int(input())
-#     student_room = []
-#     for i in range(N):
-#         student_room += [[*map(int, input().split())]]
-#     for i in range(N):
-#         if student_room[i][0]%2 == 1:
-#             student_room[i][0] = (student_room[i][0]+1)//2
-#         else:
-#             student_room[i][0] = student_room[i][0]//2
-#         if student_room[i][1]%2 == 1:
-#             student_room[i][1] = (student_room[i][1]+1)//2
-#         else:
-#             student_room[i][1] = student_room[i][1]//2
-#         if student_room[i][1] < student_room[i][0]:
-#             student_room[i][0], student_room[i][1] =  student_room[i][1], student_room[i][0]
-
-#     time = [0]*201
-#     for i in range(N):
-#         for j in range(student_room[i][0], student_room[i][1]+1):
-#             time[j] += 1
-#     print(f'#{tc} {max(time)}')
-
-
-                
-
