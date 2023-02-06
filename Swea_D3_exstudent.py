a65545b72cfa2f96e76a4d41c715296b097d5a18
정호윤 <jhy1812@naver.com>
2023-02-06 10:55:17 +0900

    주석

--- Swea_D3_exstudent.py
+++ Swea_D3_exstudent.py
@@ -8,7 +8,7 @@ for i in range(TC):                                         # 테스트 케이�
     for j in range(len(week)):                              
         days = 0                                            # 지내는 일수 입력받을 변수
         cnt = 0                                             # 수업 받는 일수 입력받을 변수
-        if week[j] == 1:                                    # @@수업이 있는 날부터 지내야 최소 일수를 구할 수 있으므로, 수업 정보가 1일 때 반복 시작
+        if week[j] == 1:                                    # 수업이 있는 날부터 지내야 최소 일수를 구할 수 있으므로, 수업 정보가 1일 때 반복 시작
             lecture_day = week[j:]+week[:j]                 # 수업이 있는 날로 시작하는 리스트 할당
             while True:                                             
                 for k in range(len(lecture_day)):           # 일주일 분량만큼 반복 
@@ -19,8 +19,8 @@ for i in range(TC):                                         # 테스트 케이�
                         break
                 if cnt == n:                                # 위와 같은 조건 만족시 while문 종료
                     break
-        else:                                               # @@
+        else:                                               # 수업 정보가 0일 때는 다음 반복문 
             continue
 
-        check.append(days)
-    print('#%d %d'%(i+1,min(check)))
+        check.append(days)                                  # 지낸 일수 배열에 입력
+    print('#%d %d'%(i+1,min(check)))                        # 지낸 일수 중 최솟값 출력
